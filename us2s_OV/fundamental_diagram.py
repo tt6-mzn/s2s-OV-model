5f76db7782d65b1f9439913de86de00bc4e7d5c0
tt6-mzn <tt6-mzn@users.noreply.github.com>
2023-07-08 11:26:17 +0900

    us2sの変更に対応

--- us2s_OV/fundamental_diagram.py
+++ us2s_OV/fundamental_diagram.py
@@ -2,32 +2,29 @@ import sys
 import matplotlib.pyplot as plt
 import random
 import us2s_OV
+import numpy as np
 
 def main():
-	L     = 100  # レーンの長さ
 	n_0   = 3    # monitoring period
 	x_0   = 1    # 最短車間距離
 	v_0   = 4    # 車両の最高速度
-	dt    = 1    # 時間差分
-	nmax  = 1001 # シミュレーションするステップ数
 
-	density = []
-	flow = []
+	density = np.zeros(shape=(101 * 100))
+	flow = np.zeros(shape=(101 * 100))
  
 	# 密度を変えて、複数の初期条件からflowを計算
-	for K in range(0, 101):
-		for _ in range(100): # 各密度について100回ずつ計算
+	id = 0  # 各シミュレーションに割り振る番号
+	for K in range(0, us2s_OV.K_MAX + 1):
+		for _ in range(1): # 各密度について100回ずつ計算
 			sys.stdout.write("\rK = {}, ({}/{})".format(K, _, 100))
 			sys.stdout.flush()
 			# 車両の初期位置をランダムに生成
-			x_init = sorted(random.sample([i for i in range(L)], K))
-			model = us2s_OV.us2s_OV(L, K, n_0, x_0, v_0, dt, x_init, nmax)
+			x_init = sorted(random.sample([i for i in range(us2s_OV.L)], K))
+			model = us2s_OV.us2s_OV(K, n_0, x_0, v_0, x_init)
 			model.simulate()
-			density.append(model.density())
-			flow.append(model.flow(800, 1000))
-	
-	# for d, f in zip(density, flow):
-	# 	print(d, f)
+			density[id] = model.density()
+			flow[id] = model.flow(800, 1000)
+			id += 1
 	
 	plt.figure(figsize=(6.4, 6.4))
 	plt.grid()
