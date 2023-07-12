84b4893d349b3665ce98aa3ac966412a09f5c395
tt6-mzn <tt6-mzn@users.noreply.github.com>
2023-07-13 08:28:33 +0900

    シミュレーションの整備

--- us2s_OV/fundamental_diagram.py
+++ us2s_OV/fundamental_diagram.py
@@ -4,19 +4,21 @@ import random
 import us2s_OV
 import numpy as np
 
+M = 1000  # 各密度についてシミュレーションする回数
+
 def main():
 	n_0 = 3    # monitoring period
 	x_0 = 1    # 最短車間距離
 	v_0 = 4    # 車両の最高速度
 
-	density = np.zeros(shape=(101 * 100))
-	flow    = np.zeros(shape=(101 * 100))
+	density = np.zeros(shape=((us2s_OV.K_MAX+1) * M))
+	flow    = np.zeros(shape=((us2s_OV.K_MAX+1) * M))
  
 	# 密度を変えて、複数の初期条件からflowを計算
 	id = 0  # 各シミュレーションに割り振る番号
 	for K in range(0, us2s_OV.K_MAX + 1):
-		for _ in range(1): # 各密度について100回ずつ計算
-			sys.stdout.write("\rK = {}, ({}/{})".format(K, _, 100))
+		for _ in range(M):
+			sys.stdout.write("\rK = {:3d}, ({:4d}/{:4d})".format(K, _+1, M))
 			sys.stdout.flush()
 			# 車両の初期位置をランダムに生成
 			x_init = sorted(random.sample([i for i in range(us2s_OV.L)], K))
