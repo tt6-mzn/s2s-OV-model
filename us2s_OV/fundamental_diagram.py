6af2ae07823aa48bdd03ba8cd280904fc2e29ee7
tt6-mzn <tt6-mzn@users.noreply.github.com>
2023-07-13 23:02:15 +0900

    調整して図完成

--- us2s_OV/fundamental_diagram.py
+++ us2s_OV/fundamental_diagram.py
@@ -28,7 +28,7 @@ def main():
 		ax.set_xlabel("Density")
 		ax.set_ylabel("Flow")
 
-	M = [500 if i <= 60 else 10 for i in range(L+1)]  # 各密度についてシミュレーションする回数
+	M = [10 if i <= 60 else 5 for i in range(L+1)]  # 各密度についてシミュレーションする回数
 
 	for v_0, n_0, axi in zip(v_0_list, n_0_list, axs):
 		density = []
@@ -36,14 +36,34 @@ def main():
 	
 		# 密度を変えて、複数の初期条件からflowを計算
 		for K in range(0, L + 1):
-			for _ in range(M[K]):
+			if K <= 50:
 				# 均等に車両を配置した場合を試す
-				if _ == 0 and K < 50:
-					x_init = sorted([int((i * L) / K) for i in range(K)])
+				x_init = sorted([int((i * L) / K) for i in range(K)])
+				model = us2s_OV_CA.us2s_OV_CA(L, K, n_0, v_0, x_init, n_max)
+				model.simulate()
+				density.append(model.density())
+				flow.append(model.flow(800, 1000))
+			if  K < 40:
+				# そこそこ均等に車両を配置した場合を試す
+				for _ in range(500):
+					sys.stdout.write("\rK = {:3d}, ({:4d}/{:4d})".format(K, _+1, 500))
+					sys.stdout.flush()
+					x_init = sorted([int((i * L) / K) + random.sample([0, 1], 1)[0] for i in range(K)])
 					model = us2s_OV_CA.us2s_OV_CA(L, K, n_0, v_0, x_init, n_max)
 					model.simulate()
 					density.append(model.density())
 					flow.append(model.flow(800, 1000))
+				# あらめに均等に車両を配置した場合を試す
+				for _ in range(500):
+					sys.stdout.write("\rK = {:3d}, ({:4d}/{:4d})".format(K, _+1, 500))
+					sys.stdout.flush()
+					x_init = sorted([int((i * L) / K) + random.sample([0, 1, 2], 1)[0] for i in range(K)])
+					model = us2s_OV_CA.us2s_OV_CA(L, K, n_0, v_0, x_init, n_max)
+					model.simulate()
+					density.append(model.density())
+					flow.append(model.flow(800, 1000))
+			# ランダムに試す
+			for _ in range(M[K]):
 				sys.stdout.write("\rK = {:3d}, ({:4d}/{:4d})".format(K, _+1, M[K]))
 				sys.stdout.flush()
 				# 車両の初期位置をランダムに生成
@@ -53,32 +73,14 @@ def main():
 				density.append(model.density())
 				flow.append(model.flow(800, 1000))
 		
-		with open("./data/v0={}, n0={}.txt".format(v_0, n_0), 'w', encoding='utf-8') as f:
+		with open("./test/v0={}, n0={}.txt".format(v_0, n_0), 'w', encoding='utf-8') as f:
 			for de, fl in zip(density, flow):
 				f.write("{}, {}\n".format(de, fl))
 		
 		axi.scatter(density, flow, s=3)
-		
-		# plt.figure(figsize=(6.4, 6.4))
-		# plt.title("v0dt={}, n0={}".format(v_0, n_0))
-		# plt.xlabel("Density")
-		# plt.ylabel("Flow")
-		# plt.xticks([0.1 * i for i in range(0, 11)])
-		# plt.yticks([0.1 * i for i in range(0, 11)])
-		# plt.grid()
-		# plt.xlim(0.0, 1.0)
-		# plt.ylim(0.0, 1.0)
-		# plt.scatter(
-		# 	x=density,
-		# 	y=flow,
-		# 	s=2,
-		# 	marker='o',
-		# )
-		# plt.savefig("./img/v0dt={}, n0={}.png".format(v_0, n_0))
-	
 		print()
 	
-	plt.savefig("./img/fundamental diagram.png")
+	plt.savefig("./test/fundamental diagram.png")
 
 if __name__ == "__main__":
     main()
